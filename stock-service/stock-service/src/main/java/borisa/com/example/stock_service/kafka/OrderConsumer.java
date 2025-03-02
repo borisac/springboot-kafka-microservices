6f7f7511495fe2801a979438baefeb7be9cedc1e
Borisa <borisa.cajic@gmail.com>
2025-03-03 00:01:32 +0100

    Enabling/creating email consumer

---
+++ stock-service/stock-service/src/main/java/borisa/com/example/stock_service/kafka/OrderConsumer.java
@@ -0,0 +1,26 @@
+package borisa.com.example.stock_service.kafka;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import borisa.com.example.base_domains.dto.OrderEvent;
+
+@Service
+public class OrderConsumer {
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(OrderConsumer.class);
+
+	@KafkaListener(topics = "${spring.kafka.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+	public void conusme(OrderEvent event) {
+
+		LOGGER.info(String.format("Order event received in stock service => %S", event.toString()));
+		
+		//here we can save order event data to db, but not now.just implementing communication
+		
+		
+
+	}
+
+}
